6446e51b157b712c8d170339d53961610f7a022a
teacherIan <ian_malloy@ssis.asia>
2023-12-03 10:37:37 +0800

    lowered shadow-mapSize

--- src/Light.jsx
+++ src/Light.jsx
@@ -5,7 +5,7 @@ export default function Light(props) {
       position={[props.x, props.y, props.z]}
       castShadow
       distance={1000}
-      shadow-mapSize={[1024 * 2, 1024 * 2]}
+      shadow-mapSize={[1024, 1024]} //higher map sizes crash on some phones
       shadow-camera-top={300}
       shadow-camera-right={200}
       shadow-camera-bottom={-200}
