cc794d8ab4405367d56203e3f080f7f4f7c9898a
teacherIan <ian_malloy@ssis.asia>
2023-12-04 22:02:21 +0800

    updates...

--- src/lobby/Mov.jsx
+++ src/lobby/Mov.jsx
@@ -2,8 +2,18 @@ import vid from '../assets/theBox.m4v';
 import './lobby.css';
 
 export default function Mov({ setPlayVideo }) {
+  const orientation = Screen.orientation;
+
+  console.log(orientation);
   return (
-    <video onEnded={() => setPlayVideo(false)} className="video" autoPlay>
+    <video
+      onEnded={() => setPlayVideo(false)}
+      controls
+      className="video"
+      autoPlay
+      muted
+      preload="auto"
+    >
       <source src={vid} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
